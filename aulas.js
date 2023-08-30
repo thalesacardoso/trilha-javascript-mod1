8fc3af3b326f0768e60ec115aba43b8747b17922
thalesacardoso <thales.dev@hotmail.com>
2023-08-30 10:17:31 -0300

    início da formação javascript

---
+++ aulas.js
@@ -0,0 +1,64 @@
+/*
+COMENTANDO CÓDIGO
+//  COMENTÁRIO EM LINHA 
+
+DECLARAÇÃO DE VARIÁVEIS
+    let nomeDaVariavel = "texto"; 
+
+DECLARAÇÃO DE CONSTANTES
+    const nomeDaConstante = 3.16;
+
+TIPOS DE ERROS
+    Assignment to constant variable.  ( aparece quando esta tentando alterar o valor de uma constante )
+
+COMANDOS DE SAÍDA
+    console.log(nomeDaConstante);  VAI MOSTRAR O QUE ESTA NA VARIÁVEL
+    console.log("nomeDaConstante");  VAI MOSTRAR EXATAMENTE O QUE ESTÁ DENTRO DAS "ASPAS"
+
+FUNÇÕES NATIVAS
+    gastoTotal.toFixed(2)  // DEIXA O NUMERO COM 2 CASAS DECIMAIS
+
+
+OPERADORES ARITMÉTICOS
+    +  SOMA
+    -  SUBTRAÇÃO
+    *  MULTIPLICAÇÃO
+    /  DIVISÃO
+    %  RESTO DA DIVISÃO
+
+OPERADORES RELACIONAIS / CONDICIONAIS
+
+
+ESTRUTURAS CONDICIONAIS
+
+    if(numero === 1){
+        executa se for verdadeiro / true
+    }
+
+    if(expressão){
+        aqui é o bloco que será executado se a condição for verdadeira;
+    } else {
+        aqui é o bloco que será executado se a condição for falsa;
+    }
+
+    if(expressão){
+        aqui é o bloco que será executado no 1º teste;
+    } else if (expressão) {
+        aqui é o bloco que será executado no 2º teste;
+    } else {
+        aqui é o bloco que será executado se a condição for falsa;
+    }
+*/
+
+
+
+let precoCombustivel = 5.0;
+let gastokm = 12;
+let distancia = 100;
+let total
+let gastoTotal
+
+total = distancia / gastokm;
+gastoTotal = total * precoCombustivel;
+
+console.log(gastoTotal.toFixed(2));
