c1c983516da1ad0336d084cb66f4580460a28632
thalesacardoso <thales.dev@hotmail.com>
2023-08-30 10:21:44 -0300

    exercício 01

---
+++ exercicio-01.js
@@ -0,0 +1,10 @@
+/*
+Faça um programa para calcular o valor de uma viagem.
+
+você terá 3 variáveis. sendo elas:
+1 - Preço do combustível;
+2 - Gasto médio de combustível do carro por KM;
+3 - Distância em KM da viagem;
+
+Imprima no console o valor que será gasto de combustível para realizar esta viagem.
+*/
