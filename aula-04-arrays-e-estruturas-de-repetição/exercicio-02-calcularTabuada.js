f543b6377097776100e14358f9ffb564887de491
thalesacardoso <thales.dev@hotmail.com>
2023-09-01 12:33:16 -0300

    Aula 04 - exercicio 02

---
+++ aula-04-arrays-e-estruturas-de-repetição/exercicio-02-calcularTabuada.js
@@ -0,0 +1,11 @@
+// Exercícios de tabuada
+
+// Declaração de variáveis
+let numero = 4;
+let total = 0;
+
+// Laços de repetição com o for
+for (let i = 1; i <= 10; i++) {
+    total = numero * i;
+    console.log(`A multiplicação de ${numero} x ${i} = ${total}`)
+};
